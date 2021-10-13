bb9083a767ac5fabad007785af86c2e34c4c5dda
KarimAmrM <karimm@silverkeytech.com>
2021-10-14 00:00:41 +0200

    Added custom pizza ordering

--- PizzaUI/PizzaUI/Pages/Index.cshtml.cs
+++ PizzaUI/PizzaUI/Pages/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace PizzaUI.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly HttpClient _httpClient;
         public List<Pizza>? Menu { get; set; }
+        public List<Topping>? Toppings { get; set; }
         public string? PictureLocation { get; set; }
         public double PizzaPrice { get; set; }
         public List<Pizza>? PizzasSelected { get; set; }
@@ -22,7 +23,7 @@ namespace PizzaUI.Pages
         [BindProperty]
         [Required]
         public string ClientName { get; set; }
-       
+
         public IndexModel(ILogger<IndexModel> logger, HttpClient httpClient)
         {
             _logger = logger;
@@ -32,7 +33,7 @@ namespace PizzaUI.Pages
         public async Task OnGetAsync()
         {
             Menu = await _httpClient.GetFromJsonAsync<List<Pizza>>("http://localhost:5000/menu");
-            if (!HttpContext.Session.Keys.Contains("menu")) 
+            if (!HttpContext.Session.Keys.Contains("menu"))
             {
                 SessionHelper.SetObjectAsJson(HttpContext.Session, "menu", Menu);
             };
@@ -43,7 +44,7 @@ namespace PizzaUI.Pages
         {
             ViewData["PizzaSize"] = PizzaSize;
             Menu = SessionHelper.GetObjectFromJson<List<Pizza>>(HttpContext.Session, "menu");
-            if (Menu != null) 
+            if (Menu != null)
             {
                 var pizzaToAdd = from pizza in Menu
                                  where pizza.Id == pizzaID
@@ -65,7 +66,7 @@ namespace PizzaUI.Pages
             }
         }
 
-        public async Task<IActionResult> OnPostCompleteOrderAsync(string clientName) 
+        public async Task<IActionResult> OnPostCompleteOrderAsync(string clientName)
         {
             PizzasSelected = SessionHelper.GetObjectFromJson<List<Pizza>>(HttpContext.Session, "cart");
             if (PizzasSelected != null)
@@ -75,6 +76,43 @@ namespace PizzaUI.Pages
                 HttpContext.Session.Clear();
             }
             return RedirectToPage("Index");
+        }
+
+        public async Task<PartialViewResult> OnGetChooseToppgingsModalPartialAsync()
+        {
+            this.Toppings = await _httpClient.GetFromJsonAsync<List<Topping>>("http://localhost:5000/toppings");
+            return new PartialViewResult
+            {
+                ViewName = "_ChooseToppings",
+                ViewData = new Microsoft.AspNetCore.Mvc.ViewFeatures.ViewDataDictionary<List<Topping>>(ViewData, this.Toppings)
+            };
+        }
+
+        public async Task OnPostAddCustomPizzaAsync(List<int> selectedToppingsIds, string pizzaName)
+        {
+            Toppings = await _httpClient.GetFromJsonAsync<List<Topping>>("http://localhost:5000/toppings");
+            PizzasSelected = SessionHelper.GetObjectFromJson<List<Pizza>>(HttpContext.Session, "cart");
+            if (PizzasSelected == null)
+            {
+                PizzasSelected = new();
+            }
+            if (PizzaSize != null)
+            {
+                List<Topping> selectedToppings = new();
+                foreach (int id in selectedToppingsIds)
+                {
+                    var toppingToAdd = from topping in Toppings
+                                       where topping.Id == id
+                                       select topping;
+                    if (toppingToAdd != null)
+                    {
+                        selectedToppings.Add(toppingToAdd.First());
+                    }
+                }
+                Pizza customPizzaToAdd = new(-1, pizzaName, selectedToppings, PizzaSize, true);
+                PizzasSelected.Add(customPizzaToAdd);
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", PizzasSelected);
+            }
         }
     }
 }
